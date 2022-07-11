50a37df99a79b19e0efe27d3b840ebd5f4bc9d68
Дмитрий Шашев <DmitryShashev@outlook.com>
2022-07-11 10:32:10 +0700

    Refactoring

--- src/MegafonATS.Client/IMegafonAtsClient.cs
+++ src/MegafonATS.Client/IMegafonAtsClient.cs
@@ -4,15 +4,97 @@ namespace MegafonATS.Client
 {
     public interface IMegafonAtsClient
     {
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС для получения сотрудников.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Коллекцию отделов</returns>
         Task<ClientResult<IEnumerable<UserModel>>> AccountsAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС для получения отделов для конкретного сотрудника
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
+        /// <param name="cancellationToken">Коллекцию отделов</param>
+        /// <returns>Коллекцию отделов</returns>
         Task<ClientResult<IEnumerable<GroupModel>>> GroupsAsync(string user, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС для получения отделов
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Коллекцию отделов</returns>
         Task<ClientResult<IEnumerable<GroupModel>>> GroupsAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Команда необходимая для того, чтобы инициировать звонок от менеджера клиенту. В результате
+        /// успешного выполнения команды, Виртуальная АТС сделает сначала звонок на телефон менеджера,
+        /// а потом соединит его с клиентом.Команда может использоваться, например, для звонка по клику
+        /// на номер клиента в вашей CRM или базе данных.
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
+        /// <param name="phoneNumber">"номер телефона клиента"</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Уникальный идентификатор звонка</returns>
         Task<ClientResult<CurrentCallModel>> MakeCallAsync(string user, string phoneNumber, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Команда необходима для того, чтобы получить из Виртуальной АТС историю звонков за нужный период времени.
+        /// </summary>
+        /// <param name="period">период времени за который нужно выгрузить данные.</param>
+        /// <param name="type">тип звонка</param>
+        /// <param name="limit">результат должен содержать не более, чем limit записей </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Коллекцию объектов с информацией о звонках</returns>
         Task<ClientResult<IEnumerable<CallModel>>> HistoryAsync(FilterPeriod period, FilterCallType type, int limit = int.MaxValue, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Команда необходима для того, чтобы получить из Виртуальной АТС историю звонков за нужный период времени.
+        /// </summary>
+        /// <param name="start">начало периода для выгрузки данных</param>
+        /// <param name="end">окончание периода для выгрузки данных</param>
+        /// <param name="type">тип звонка</param>
+        /// <param name="limit">результат должен содержать не более, чем limit записей</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Коллекцию объектов с информацией о звонках</returns>
         Task<ClientResult<IEnumerable<CallModel>>> HistoryAsync(DateTime start, DateTime end, FilterCallType type, int limit = int.MaxValue, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС для включения / выключения приема звонков сотрудником во всех
+        /// его отделах или конкретном отделе.
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
+        /// <param name="groupId">идентификатор отдела АТС, в которомнадо выключить/включить прием звонков </param>
+        /// <param name="status">on - чтобы включить прием звонков, off - чтобы выключить прием звонков</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Информация о успехе/неуспехе операции</returns>
         Task<ClientResult> SubscribeOnCallsAsync(string user, string groupId, SubscriptionStatus status, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС для проверки факта приема звонков сотрудником в конкретном отделе
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
+        /// <param name="groupId">идентификатор отдела АТС, для которого надо выполнить проверку</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<ClientResult<StatusModel>> SubscriptionStatusAsync(string user, string groupId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС позволяет включить или выключить прием звонков сотрудником Виртуальной АТС.
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС </param>
+        /// <param name="state">true - выключен,  false - включен </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>Информация об успехе/неуспехе операции</returns>
+
         Task<ClientResult> SetDnDAsync(string user, bool state, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Запрос от CRM к Виртуальной АТС позволяет узнать включен или выключен прием звонков сотрудником Виртуальной АТС.
+        /// </summary>
+        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<ClientResult<DnDModel>> GetDnDAsync(string user, CancellationToken cancellationToken = default);
     }
 }
--- src/MegafonATS.Client/MegafonAtsClient.cs
+++ src/MegafonATS.Client/MegafonAtsClient.cs
@@ -33,40 +33,19 @@ namespace MegafonATS.Client
 
         #region IMegafonAtsClient member
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС для получения сотрудников.
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Коллекцию отделов</returns>
+
         public async Task<ClientResult<IEnumerable<UserModel>>> AccountsAsync(CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync<IEnumerable<UserModel>>(AtsCommand.accounts, new Dictionary<string, string>() { }, cancellationToken);
 
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС для получения отделов для конкретного сотрудника
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
-        /// <param name="cancellationToken">Коллекцию отделов</param>
-        /// <returns>Коллекцию отделов</returns>
+
         public async Task<ClientResult<IEnumerable<GroupModel>>> GroupsAsync(string user, CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync<IEnumerable<GroupModel>>(AtsCommand.groups, new Dictionary<string, string>() { { "user", user } }, cancellationToken);
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС для получения отделов
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Коллекцию отделов</returns>
         public async Task<ClientResult<IEnumerable<GroupModel>>> GroupsAsync(CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync<IEnumerable<GroupModel>>(AtsCommand.groups, new Dictionary<string, string>(), cancellationToken);
 
-        /// <summary>
-        /// Команда необходима для того, чтобы получить из Виртуальной АТС историю звонков за нужный период времени.
-        /// </summary>
-        /// <param name="period">период времени за который нужно выгрузить данные.</param>
-        /// <param name="type">тип звонка</param>
-        /// <param name="limit">результат должен содержать не более, чем limit записей </param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Коллекцию объектов с информацией о звонках</returns>
+
         public async Task<ClientResult<IEnumerable<CallModel>>> HistoryAsync(FilterPeriod period, FilterCallType type, int limit = int.MaxValue, CancellationToken cancellationToken = default) =>
              await ProcessResponseAsync<IEnumerable<CallModel>>(AtsCommand.history,
                  new Dictionary<string, string>()
@@ -76,15 +55,7 @@ namespace MegafonATS.Client
                 { "limit", limit.ToString() }
             }, cancellationToken);
 
-        /// <summary>
-        /// Команда необходима для того, чтобы получить из Виртуальной АТС историю звонков за нужный период времени.
-        /// </summary>
-        /// <param name="start">начало периода для выгрузки данных</param>
-        /// <param name="end">окончание периода для выгрузки данных</param>
-        /// <param name="type">тип звонка</param>
-        /// <param name="limit">результат должен содержать не более, чем limit записей</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Коллекцию объектов с информацией о звонках</returns>
+
         public async Task<ClientResult<IEnumerable<CallModel>>> HistoryAsync(DateTime start, DateTime end, FilterCallType type, int limit = int.MaxValue, CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync<IEnumerable<CallModel>>(AtsCommand.history,
                 new Dictionary<string, string>()
@@ -95,60 +66,27 @@ namespace MegafonATS.Client
                 { "limit", limit.ToString() }
             }, cancellationToken);
 
-        /// <summary>
-        /// Команда необходимая для того, чтобы инициировать звонок от менеджера клиенту. В результате
-        /// успешного выполнения команды, Виртуальная АТС сделает сначала звонок на телефон менеджера,
-        /// а потом соединит его с клиентом.Команда может использоваться, например, для звонка по клику
-        /// на номер клиента в вашей CRM или базе данных.
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
-        /// <param name="phoneNumber">"номер телефона клиента"</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Уникальный идентификатор звонка</returns>
+
         public async Task<ClientResult<CurrentCallModel>> MakeCallAsync(string user, string phoneNumber, CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync<CurrentCallModel>(AtsCommand.makeCall, new Dictionary<string, string>() { { "user", user }, { "phone", phoneNumber } }, cancellationToken);
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС для включения / выключения приема звонков сотрудником во всех
-        /// его отделах или конкретном отделе.
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
-        /// <param name="groupId">идентификатор отдела АТС, в которомнадо выключить/включить прием звонков </param>
-        /// <param name="status">on - чтобы включить прием звонков, off - чтобы выключить прием звонков</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Информация о успехе/неуспехе операции</returns>
-        public async Task<ClientResult> SubscribeOnCallsAsync(string user, string groupId, SubscriptionStatus status, CancellationToken cancellationToken = default)
-        {
-            return await ProcessResponseAsync(AtsCommand.subscribeOnCalls, new Dictionary<string, string>() { { "user", user }, { "group_id", groupId }, { "status", status.ToString().ToLower() } }, cancellationToken);
-        }
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС для проверки факта приема звонков сотрудником в конкретном отделе
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
-        /// <param name="groupId">идентификатор отдела АТС, для которого надо выполнить проверку</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        public async Task<ClientResult> SubscribeOnCallsAsync(string user, string groupId, SubscriptionStatus status, CancellationToken cancellationToken = default) =>
+            await ProcessResponseAsync(AtsCommand.subscribeOnCalls,
+                new Dictionary<string, string>()
+            {
+                { "user", user },
+                { "group_id", groupId },
+                { "status", status.ToString().ToLower() }
+            }, cancellationToken);
+
+
         public async Task<ClientResult<StatusModel>> SubscriptionStatusAsync(string user, string groupId, CancellationToken cancellationToken = default) =>
              await ProcessResponseAsync<StatusModel>(AtsCommand.subscriptionStatus, new Dictionary<string, string>() { { "user", user }, { "group_id", groupId } }, cancellationToken);
 
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС позволяет включить или выключить прием звонков сотрудником Виртуальной АТС.
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС </param>
-        /// <param name="state">true - выключен,  false - включен </param>
-        /// <param name="cancellationToken"></param>
-        /// <returns>Информация об успехе/неуспехе операции</returns>
         public async Task<ClientResult> SetDnDAsync(string user, bool state, CancellationToken cancellationToken = default) =>
             await ProcessResponseAsync(AtsCommand.set_dnd, new Dictionary<string, string>() { { "user", user }, { "state", state.ToString().ToLower() } }, cancellationToken);
 
-
-        /// <summary>
-        /// Запрос от CRM к Виртуальной АТС позволяет узнать включен или выключен прием звонков сотрудником Виртуальной АТС.
-        /// </summary>
-        /// <param name="user">идентификатор сотрудника Виртуальной АТС</param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
         public async Task<ClientResult<DnDModel>> GetDnDAsync(string user, CancellationToken cancellationToken = default)
         {
             return await ProcessResponseAsync<DnDModel>(AtsCommand.get_dnd, new Dictionary<string, string>() { { "user", user } }, cancellationToken);
